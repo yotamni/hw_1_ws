6a521bd12abd2f2d0525750437a00857c3b298a1
Yotam Nitzan <yotamni@gmail.com>
2017-05-05 21:16:46 +0300

    test

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/',function(req,res){
 });
 */
 app.get('/',function(req,res){
-    res.json({pageName:"home1"});
+    res.json({pageName:"home"});
 });
 app.post('/products',function(req,res){
     res.json({pageName:'products'});
